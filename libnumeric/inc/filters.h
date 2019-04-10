f3963ebef9c7a19c8a60597bb50f7713c1a35e0a
Simone Comari <simone.comari2@unibo.it>
2019-04-10 18:02:00 +0200

    fixed bug in 1st ord lp filter + added 2nd ord lp filter

--- libnumeric/inc/filters.h
+++ libnumeric/inc/filters.h
@@ -1,12 +1,15 @@
 /**
  * @file filters.h
  * @author Simone Comari
- * @date 15 Mar 2019
+ * @date 10 Apr 2019
  * @brief This file include filters implementations.
  */
 
 #ifndef GRABCOMMON_LIBNUMERIC_FILTERS_H
 #define GRABCOMMON_LIBNUMERIC_FILTERS_H
+
+#include <math.h>
+#include <vector>
 
 /**
  * @brief Namespace for GRAB numeric library.
@@ -14,7 +17,7 @@
 namespace grabnum {
 
 /**
- * @brief A low-pass filter implementation.
+ * @brief A first-order low-pass filter implementation.
  */
 class LowPassFilter
 {
@@ -33,31 +36,42 @@ class LowPassFilter
    */
   LowPassFilter(const double& cutoff_freq, const double& sampling_time_sec,
                 const double& init_value);
+  virtual ~LowPassFilter() {}
 
   /**
-   * @brief Set cutoff frequency.
+   * @brief Set LP filter cutoff frequency.
    * @param cutoff_freq [Hz] Cutoff frequency of low-pass filter.
    */
-  void SetCutoffFreq(const double& cutoff_freq) { cutoff_freq_ = cutoff_freq; }
+  void SetCutoffFreq(const double& cutoff_freq, bool reset = false);
   /**
-   * @brief Set sampling time.
+   * @brief Set LP filter sampling time.
    * @param sampling_time_sec Sampling time in seconds.
    */
-  void SetDeltaTime(const double& sampling_time_sec) { Ts_ = sampling_time_sec; }
+  void SetSamplingTime(const double& sampling_time_sec, bool reset = false);
+  /**
+   * @brief Set LP filter time constant.
+   * @param time_const_sec Time constant in seconds.
+   */
+  void SetTimeConstant(const double& time_const_sec, bool reset = false);
 
   /**
-   * @brief Get cutoff frequency.
+   * @brief Get LP filter cutoff frequency.
    * @return [Hz] Cutoff frequency of low-pass filter.
    */
   double GetCutoffFreq() const { return cutoff_freq_; }
   /**
-   * @brief Get sampling time.
+   * @brief Get LP filter sampling time.
    * @return Sampling time in seconds.
    */
-  double GetDeltaTime() const { return Ts_; }
+  double GetSamplingTime() const { return Ts_; }
+  /**
+   * @brief Get LP filter time constant.
+   * @return LP filter time constant in seconds.
+   */
+  double GetTimeConstant() const { return Tf_; }
   /**
    * @brief Get latest filtered value.
-   * @return Latest filtered value.
+   * @return Latest filtered value.h
    */
   double GetLatestFilteredValue() const { return filtered_value_; }
 
@@ -66,19 +80,85 @@ class LowPassFilter
    * @param raw_value New signal value.
    * @return Filtered value.
    */
-  double Filter(const double& raw_value);
+  virtual double Filter(const double& raw_value);
   /**
    * @brief Reset filter, clearing its history.
    */
   void Reset() { initialized_ = false; }
 
- private:
+ protected:
   double cutoff_freq_; // [Hz]
   double Ts_;          // [sec]
-  double alpha_;
+  double Tf_;          // [sec]
+  double alpha_ = 0.0;
 
   bool initialized_;
   double filtered_value_;
+
+  virtual void UpdateConstant(bool reset = false);
+};
+
+class SecondOrderLPFilter: LowPassFilter
+{
+ public:
+  /**
+   * @brief LowPassFilter constructor.
+   * @param cutoff_freq [Hz] Cutoff frequency of low-pass filter.
+   * @param sampling_time_sec Sampling time in seconds.
+   */
+  SecondOrderLPFilter(const double& cutoff_freq1, const double& cutoff_freq2,
+                      const double& sampling_time_sec);
+  /**
+   * @brief LowPassFilter full constructor.
+   * @param cutoff_freq [Hz] Cutoff frequency of low-pass filter.
+   * @param sampling_time_sec Sampling time in seconds.
+   * @param init_value Initial value of the signal to be filtered.
+   */
+  SecondOrderLPFilter(const double& cutoff_freq, const double& cutoff_freq2,
+                      const double& sampling_time_sec, const double& init_value);
+
+  /**
+   * @brief Set LP filter cutoff frequencies.
+   * @param cutoff_freq [Hz] Cutoff frequencies of low-pass filter.
+   */
+  void SetCutoffFreq(const std::vector<double>& cutoff_freq, bool reset = false);
+  /**
+   * @brief Set LP filter time constants.
+   * @param time_const_sec Time constants in seconds.
+   */
+  void SetTimeConstant(const std::vector<double>& time_const_sec, bool reset = false);
+
+  /**
+   * @brief Get LP filter cutoff frequencies.
+   * @return [Hz] Cutoff frequencies of low-pass filter.
+   */
+  std::vector<double> GetCutoffFreq() const { return {cutoff_freq_, cutoff_freq2_}; }
+  /**
+   * @brief Get LP filter time constants.
+   * @return LP filter time constants in seconds.
+   */
+  std::vector<double> GetTimeConstant() const { return {Tf_, Tf2_}; }
+
+  /**
+   * @brief Filter a new value of a signal.
+   * @param raw_value New signal value.
+   * @return Filtered value.
+   */
+  double Filter(const double& raw_value) override;
+
+ private:
+  double cutoff_freq2_; // [Hz]
+  double Tf2_;          // [sec]
+  double beta_ = 0.0;
+
+  double prev_filt_value_ = 0.0;
+
+  void UpdateConstant(bool reset = false) override;
+
+  using LowPassFilter::GetCutoffFreq;
+  using LowPassFilter::GetTimeConstant;
+  using LowPassFilter::SetCutoffFreq;
+  using LowPassFilter::SetTimeConstant;
 };
 
 } // end namespace grabnum
