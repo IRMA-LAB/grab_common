0134b3ee1747cb48bd4cbe7f9c95655c83ac9f92
Simone Comari <simone.comari2@unibo.it>
2019-07-17 17:28:05 +0200

    fixed indexing bug

--- libgeom/src/homogeneous_transf.cpp
+++ libgeom/src/homogeneous_transf.cpp
@@ -18,8 +18,8 @@ grabnum::Matrix4d BuildHomogeneousTransf(const grabnum::Matrix3d& rot_mat,
                                          const grabnum::Vector3d& transl_vect)
 {
   grabnum::Matrix4d homg_mat(1.0); // init as unitary diagonal matrix
-  homg_mat.SetBlock<3, 3>(0, 0, rot_mat);
-  homg_mat.SetBlock<3, 1>(0, 3, transl_vect);
+  homg_mat.SetBlock<3, 3>(1, 1, rot_mat);
+  homg_mat.SetBlock<3, 1>(1, 4, transl_vect);
   return homg_mat;
 }
 
@@ -36,20 +36,20 @@ void SetHomgTransfTransl(const grabnum::Vector3d& transl_vect,
 
 grabnum::Matrix3d GetHomgTransfRot(const grabnum::Matrix4d& homg_transf)
 {
-  return homg_transf.GetBlock<3, 3>(0, 0);
+  return homg_transf.GetBlock<3, 3>(1, 1);
 }
 
 grabnum::Vector3d GetHomgTransfTransl(const grabnum::Matrix4d& homg_transf)
 {
-  return homg_transf.GetBlock<3, 1>(0, 3);
+  return homg_transf.GetBlock<3, 1>(1, 4);
 }
 
 grabnum::Matrix4d InverseTransformation(const grabnum::Matrix4d& homg_transf)
 {
   grabnum::Matrix4d inv_trans(1.0); // init as unitary diagonal matrix
   grabnum::Matrix3d rot_mat_inv = GetHomgTransfRot(homg_transf).Transpose();
-  inv_trans.SetBlock<3, 3>(0, 0, rot_mat_inv);
-  inv_trans.SetBlock<3, 1>(0, 3, -rot_mat_inv * GetHomgTransfTransl(homg_transf));
+  inv_trans.SetBlock<3, 3>(1, 1, rot_mat_inv);
+  inv_trans.SetBlock<3, 1>(1, 4, -rot_mat_inv * GetHomgTransfTransl(homg_transf));
   return inv_trans;
 }
 
