36c861db42cb68edecd599e52c13b73960b798bc
Simone Comari <simone.comari@unibo.it>
2022-04-08 16:19:56 +0200

    increased macro FOR loop to max 11 iterations

--- grabcommon.h
+++ grabcommon.h
@@ -71,15 +71,16 @@
 #define FE_8(WHAT, X, ...) WHAT(X) FE_7(WHAT, __VA_ARGS__)
 #define FE_9(WHAT, X, ...) WHAT(X) FE_8(WHAT, __VA_ARGS__)
 #define FE_10(WHAT, X, ...) WHAT(X) FE_9(WHAT, __VA_ARGS__)
+#define FE_11(WHAT, X, ...) WHAT(X) FE_10(WHAT, __VA_ARGS__)
 //... repeat as needed
-#define GET_MACRO(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, NAME, ...) NAME
+#define GET_MACRO(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, NAME, ...) NAME
 // @endcond
 /**
  * @brief A for loop macro implementation.
- * @warning It works up to 10 iterations, to add more you must extend the list above.
+ * @warning It works up to 11 iterations, to add more you must extend the list above.
  */
 #define FOR_EACH(action, ...)                                                            \
-  GET_MACRO(__VA_ARGS__, FE_10, FE_9, FE_8, FE_7, FE_6, FE_5, FE_4, FE_3, FE_2, FE_1)    \
+  GET_MACRO(__VA_ARGS__, FE_11, FE_10, FE_9, FE_8, FE_7, FE_6, FE_5, FE_4, FE_3, FE_2, FE_1)    \
   (action, __VA_ARGS__)
 
 //---------------------- Generic enums -----------------------------------------------//
