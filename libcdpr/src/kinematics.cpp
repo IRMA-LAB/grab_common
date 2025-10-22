ba9681e64d8c88facbc5177d5ebf208a71abb6dc
Michele Angelini <michele.angelini2@unibo.it>
2025-10-22 15:12:50 +0200

    refact: updated state est weights

--- libcdpr/src/kinematics.cpp
+++ libcdpr/src/kinematics.cpp
@@ -356,11 +356,11 @@ namespace grabcdpr {
         VectorXd<8> swivel_noise;
         Vector3d AHRS_noise;
         for (unsigned int i = 0; i < vars.cables.size(); i++) {
-            length_noise(i + 1) = 1 / 0.004;                    // 5 mm of std
-            swivel_noise(i + 1) = 1 / (0.8 * M_PI / 180);       // 0.5 deg of std
+            length_noise(i + 1) = 1 / 0.004;                    // 4 mm of std
+            swivel_noise(i + 1) = 1 / (0.8 * M_PI / 180);       // 0.8 deg of std
         }
         for (unsigned int i = 1; i <= 3; i++)
-            AHRS_noise(i) = 1 / (1 * M_PI / 180);               // 0.8 deg of std
+            AHRS_noise(i) = 1 / (1 * M_PI / 180);               // 1 deg of std
         VectorXd<19> weights;
         weights.SetBlock<8, 1>(1, 1, length_noise);
         weights.SetBlock<8, 1>(9, 1, swivel_noise);
