8c3052134f8df46342ac2d0f5821a3fd060651b2
Michele Angelini <michele.angelini2@unibo.it>
2025-12-11 18:14:16 +0100

    refact: developing TD gouttefarde
    NB not tested!

--- libcdpr/inc/TensionDistribution.h
+++ libcdpr/inc/TensionDistribution.h
@@ -4,7 +4,7 @@
 #include "grabcommon.h"
 #include "matrix_utilities.h"
 #include "cdpr_types.h"
-
+#include <iostream>
 
 using namespace grabnum;
 
@@ -32,7 +32,7 @@ struct Index_and_limits{
 
 
 /**
- * @brief Update cables tension distribution given a certain CDPR status using 3-angle
+ * @brief Update cables tension distribution with Improved closed form method given a certain CDPR status using 3-angle
  * orientation parametrization.
  * @param[in, out] vars A reference to the robot variables structure to be updated.
  * @return a bool that states if a solution was found
@@ -57,6 +57,55 @@ bool updateCablesTensionDistribution(RobotVars& vars);
  MatrixXd<8,1> insertLimitTension7to8(const MatrixXd<7,1>& tau_h_,
                                   const uint& index_to_set,
                                   const double& TesionLimit);
+
+
+ // ________________for Gouttefarde_______________-
+
+
+ class GF
+ {
+  public:
+   GF()
+   {
+     tau_max.Fill(std::vector<double>{maxTension,maxTension,maxTension,maxTension,maxTension,maxTension,maxTension,maxTension});
+     tau_min.Fill(std::vector<double>{minTension,minTension,minTension,minTension,minTension,minTension,minTension,minTension});
+   }
+
+   /**
+    * @brief Update cables tension distribution with Gouttefarde algorithm given a certain CDPR status using 3-angle
+    * orientation parametrization.
+    * @param[in, out] vars A reference to the robot variables structure to be updated.
+    * @return a bool that states if a solution was found
+    */
+   bool updateCablesTensionGF(RobotVars& vars);
+
+   MatrixXd<1,8> calcalphas(MatrixXd<2,1> fc_, MatrixXd<8,1> qmin_, MatrixXd<8,1> qmax_,MatrixXd<1,2> ni_p_);
+
+   MatrixXd<1,8> roundalphas(MatrixXd<1,8> alpha_,int decimal);
+
+   MatrixXd<2,1> TDBaricenter(arma::mat Polygon);
+
+  private:
+
+   double maxTension=250;
+   double minTension=15;
+
+   MatrixXd<8,1> tau_max;
+   MatrixXd<8,1> tau_min;
+   MatrixXd<8,2> N;
+   arma::mat Polygon;
+
+
+   int ii=7;
+   int jj=8; //starting point must be optimized
+
+   double eps1=1e-8;
+   double eps2=1e-5;
+
+
+ };
+
+
 }
 
 
--- libcdpr/src/TensionDistribution.cpp
+++ libcdpr/src/TensionDistribution.cpp
@@ -280,7 +280,7 @@ MatrixXd<8,1> insertLimitTension7to8(const MatrixXd<7,1>& tau_h_,
 
 }
 
-}
+
 /**
 arma::vec calcCableTensionDistribution(const arma::mat& geom_jacobian,
                                        const Vector6d& ext_load,
@@ -334,4 +334,340 @@ arma::vec calcCableTensionDistribution(const arma::mat& geom_jacobian,
 }
 }
 */
+bool GF::updateCablesTensionGF(RobotVars& vars)
+  {
+
+    MatrixXd<6,8> A =vars.geom_jacobian_l.Transpose();
+    MatrixXd<6,2> Jc=A.GetCols<2>(2); //hard coded; tension controlled cables 2 and 3
+    MatrixXd<6,6> Jd;
+    Jd.SetCol(1,A.GetCol(1));
+    Jd.SetBlock(1,2,A.GetCols<5>(4));
+    MatrixXd<2,2> eye2;
+    eye2.SetIdentity();
+    N.SetBlock(1,1,-Inverse(Jd)*Jc);
+    N.SetBlock(7,1,eye2);
+
+   // external loads must be updated in cotroller
+    MatrixXd<8,1> fp;
+    fp.SetBlock(1,1,Inverse(Jd)*vars.platform.ext_load);
+    fp(7,1)=0;
+    fp(8,1)=0;
+    MatrixXd<8,1> qmax=tau_max-fp;
+    MatrixXd<8,1> qmin=tau_min-fp;
+    MatrixXd<1,2> ni=N.GetRow(ii);
+    MatrixXd<1,2> nj=N.GetRow(jj);
+    MatrixXd<2,2> NIJ;
+    NIJ.SetRow(1,ni);
+    NIJ.SetRow(2,nj);
+    MatrixXd<2,1> Q;
+    Q.SetRow(1,qmin(ii));
+    Q.SetRow(2,qmin(jj));
+    MatrixXd<2,1> fc = Inverse(NIJ)*Q;
+  //  Polygon=toArmaMat_generic(fc);
+    Polygon =arma::mat(fc.Data(), 1, 2, true).t();
+
+    MatrixXd<1, 8> In;
+    In.SetZero();
+
+    for (uint ij=1; ij<9 ; ij++)
+    {
+      MatrixXd<1, 1> first=N.GetRow(ij)*fc-qmin(ij);
+      MatrixXd<1, 1> second=N.GetRow(ij)*fc-qmax(ij);
+
+      if ( (std::abs(first(1,1))<eps2) ||  (std::abs(second(1,1))<eps2) || ((first(1,1)>0)&& (second(1,1)<0)) )
+      {
+        In(ij)=ij;
+      } else
+      {
+        In(ij)=0;
+      }
+     }
+    MatrixXd<2,1> vf=fc;
+
+    int aa=1;
+    int cnt=0;
+
+    while (aa==1)
+    {
+      ni=N.GetRow(ii);
+      nj=N.GetRow(jj);
+      MatrixXd<1,2> ni_p1;
+      ni_p1(1,1)=ni(2);
+      ni_p1(1,2)=-ni(1);
+      MatrixXd<1,2> ni_p2;
+      ni_p2(1,1)=-ni(2);
+      ni_p2(1,2)=ni(1);
+
+      MatrixXd<1, 1> cond1=nj*fc-qmin(jj);
+      MatrixXd<1, 1> cond2=nj*fc-qmax(jj);
+
+      MatrixXd<1,2> ni_p;
+      MatrixXd<1, 1> nj_nip1=nj*ni_p1.Transpose();
+      if ((std::abs(cond1(1,1)))<eps2)
+      {
+        if ((nj_nip1(1,1))>=0)
+        {
+          ni_p=ni_p1;
+        }
+        else
+        {
+          ni_p=ni_p2;
+        }
+      }
+      else if ((std::abs(cond2(1,1)))<eps2)
+      {
+        if ((nj_nip1(1,1))<=0)
+        {
+          ni_p=ni_p1;
+        }
+        else
+        {
+          ni_p=ni_p2;
+        }
+      }
+      else
+      {
+        printColor('r', "Error1: no condition true ");
+      }
+
+      MatrixXd<1,8> alpha;
+
+      alpha = calcalphas(fc, qmin, qmax, ni_p);
+      MatrixXd<1,8> ralpha;
+      ralpha= roundalphas(alpha,6);
+
+
+      double al =alpha.Max();
+
+      for (uint idx=1;idx<9;idx++)
+      {
+        if (ralpha(idx)>0)
+        {
+          if (alpha(idx)<al)
+          {
+            al=alpha(idx);
+          }
+        }
+      }
+
+      uint ll;
+
+      if ( (al==alpha.Max()) && (ralpha(alpha.MaxIdx())<=0) )
+      {
+        printColor('r', "Error2: no acceptable alpha found ");
+      }
+      else
+      {
+        fc=fc+al*ni_p.Transpose();
+        Polygon.insert_cols(Polygon.n_cols,arma::mat(fc.Data(), 1, 2, true).t());
+        for (uint idx=1;idx<9;idx++)
+        {
+          if (alpha(idx)==al)
+          {
+           ll=idx;
+          }
+        }
+
+
+        if (!(In(ll)==ll))
+        {
+          In(ll)=ll;
+          vf=fc;
+          Polygon =arma::mat(fc.Data(), 1, 2, true).t();
+          jj=ii;
+          ii=ll;
+          aa=1;
+        }
+        else
+        {
+
+          if (Norm(fc-vf)>eps1)
+          {
+            jj=ii;
+            ii=ll;
+            aa=1;
+          }
+          else
+          {
+            if( (In(1)==1) &&
+                (In(2)==2) &&
+                (In(3)==3) &&
+                (In(4)==4) &&
+                (In(5)==5) &&
+                (In(6)==6) &&
+                (In(7)==7) &&
+                (In(8)==8)  )
+            {
+              Polygon.shed_col(Polygon.n_cols-1);
+              MatrixXd<2,1> tau_c= TDBaricenter(Polygon);
+
+              MatrixXd<6,1> beq=vars.platform.ext_load-Jc*tau_c;
+              MatrixXd<6,1> tau_prelim=Inverse(Jd)*beq;
+              vars.tension_vector=arma::ones<arma::vec>(8)*(maxTension+minTension)/2;
+              vars.tension_vector(0)=tau_prelim(1);
+              vars.tension_vector(1)=tau_c(1);
+              vars.tension_vector(2)=tau_c(2);
+              vars.tension_vector(3)=tau_prelim(2);
+              vars.tension_vector(4)=tau_prelim(3);
+              vars.tension_vector(5)=tau_prelim(4);
+              vars.tension_vector(6)=tau_prelim(5);
+              vars.tension_vector(7)=tau_prelim(6);
+              return true;
+            }
+            else
+            {
+              vars.tension_vector=arma::ones<arma::vec>(8)*(maxTension+minTension)/2;
+              return false;
+            }
+          }
+        }
+      }
+
+      cnt=cnt+1;
+      if (cnt==1000)
+      {aa=0;}
+    }
+
+    vars.tension_vector=arma::ones<arma::vec>(8)*(maxTension+minTension)/2;
+    return false;
+  };
+
+
+  MatrixXd<1,8> GF::calcalphas(MatrixXd<2,1> fc_, MatrixXd<8,1> qmin_, MatrixXd<8,1> qmax_, MatrixXd<1,2> ni_p_)
+  {
+    MatrixXd<1,8> alpha;
+    alpha.SetZero();
+      for (int k=1; k<9;k++)
+      {
+        MatrixXd<1,2> nk =N.GetRow(k);
+        MatrixXd<1, 1> nk_nip=nk*ni_p_.Transpose();
+        MatrixXd<1, 1> cond1_=nk*fc_-qmin_(k);
+        MatrixXd<1, 1> cond2_=nk*fc_-qmax_(k);
+
+
+        if (nk_nip(1,1)>eps1)
+        {
+          if ( (cond1_(1,1)<0) && !(std::abs(cond1_(1,1))<eps2) )
+          {
+            alpha(k)=-cond1_(1,1)/nk_nip(1,1);
+          }
+          else if ( ( (cond1_(1,1)>0) && (cond2_(1,1)<0) ) || (std::abs(cond1_(1,1))<eps2)  )
+          {
+            alpha(k)=-cond2_(1,1)/nk_nip(1,1);
+          }
+          else
+          {
+            alpha(k)=1e16;
+          }
+        }
+        else if ( ( nk_nip(1,1)<0 ) && (std::abs(nk_nip(1,1))>eps1) )
+        {
+          if ( (cond2_(1,1)>0) && !(std::abs(cond2_(1,1))<eps1) )
+          {
+            alpha(k)=-cond2_(1,1)/nk_nip(1,1);
+          }
+          else if ( ( (cond1_(1,1)>0) && (cond2_(1,1)<0) ) || (std::abs(cond2_(1,1))<eps1) )
+          {
+            alpha(k)=-cond1_(1,1)/nk_nip(1,1);
+          }
+          else
+          {
+            alpha(k)=1e16;
+          }
+        }
+        else
+        {
+          if (k==ii)
+          {
+            alpha(k)=0;
+          }
+          else
+          {
+            alpha(k)=1e16;
+          }
+        }
+      }
+      return alpha;
+  };
+
+
+  MatrixXd<1,8> GF::roundalphas(MatrixXd<1,8> alpha_,int decimal)
+  {
+    double factor=std::pow(10.0,decimal);
+
+    for (uint idx=1; idx<9; idx++)
+    {
+    alpha_(idx)=std::round(alpha_(idx)*factor)/factor;
+    }
+    return alpha_;
+  };
+
+  MatrixXd<2,1> GF::TDBaricenter(arma::mat Polygon_)
+  {
+    MatrixXd<2,1> tau_c;
+    uint number_vertices_=Polygon_.n_cols;
+    arma::rowvec wi(number_vertices_);
+    for (uint i=0; i<number_vertices_;i++)
+    {
+      if (i==0)
+      {
+        double norm1= arma::norm(Polygon_.col(i)-Polygon_.col(number_vertices_-1));
+        double norm2= arma::norm(Polygon_.col(i)-Polygon_.col(i+1));
+
+        if (norm(Polygon_.col(i))==0)
+        {
+          wi(i)=0;
+        }
+         else
+        {
+           wi(i)=(norm1+norm2)/norm(Polygon_.col(i));
+        }
+      }
+      else if (i==number_vertices_-1)
+      {
+        double norm1= arma::norm(Polygon_.col(i)-Polygon_.col(number_vertices_-2));
+        double norm2= arma::norm(Polygon_.col(i)-Polygon_.col(0));
+        if (norm(Polygon_.col(i))==0)
+        {
+          wi(i)=0;
+        }
+        else
+        {
+          wi(i)=(norm1+norm2)/norm(Polygon_.col(i));
+        }
+      }
+      else
+      {
+        double norm1= arma::norm(Polygon_.col(i)-Polygon_.col(i-1));
+        double norm2= arma::norm(Polygon_.col(i)-Polygon_.col(i+1));
+        if (norm(Polygon_.col(i))==0)
+        {
+          wi(i)=0;
+        }
+        else
+        {
+          wi(i)=(norm1+norm2)/norm(Polygon_.col(i));
+        }
+      }
+     }
+
+    arma::vec nom ={0,0};
+     double denom=0;
+    for (uint i=0; i<number_vertices_;i++)
+    {
+      nom=nom+wi(i)*Polygon_.col(i);
+      denom=denom+wi(i);
+    }
+
+        arma::vec lambda=nom/denom;
+    tau_c(1)=lambda(0);
+    tau_c(2)=lambda(1);
+    return tau_c;
+  };
+
+
+
+
+
+}
 
